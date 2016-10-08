b74d53c856e47cc6c4f84a94c0c67df2ec811a6c
phinguyen712 <phinguyen712@gmail.com>
2016-10-08 15:29:06 -0700

    initial commit

---
+++ app/components/CountDown.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var CountDown = React.createClass({
+  render: function () {
+    return (
+        <div>
+        <CLock></CLock>
+
+        </div>
+    );
+  }
+});
+
+
+module.exports = CountDown;
